064f4d8a6e4d97cf1b3dd65e389c750ed1361927
kevinzhow <kevinchou.c@gmail.com>
2015-07-13 09:05:56 +0800

    init

---
+++ NaughtyImageView/Source/NaughtyImageView.swift
@@ -0,0 +1,141 @@
+//
+//  NaughtyImageView.swift
+//  
+//
+//  Created by kevinzhow on 15/7/13.
+//
+//
+
+import UIKit
+
+class NaughtyImageView: UIImageView {
+
+    var horizontalImages: Int!
+    
+    var verticalImages: Int!
+    
+    var floatingImage: UIImageView!
+    
+    var positionMatrix = [CGPoint(x: 0, y: 0)]
+    
+    var currentIndex = 0
+    
+    var displayLink: CADisplayLink!
+    
+    var naughtyAnimating = false
+    
+    var fps = 2
+    
+    private var frameCount = 0
+    
+    var debug = false {
+        didSet {
+            if debug {
+                clipsToBounds = false
+                layer.borderColor = UIColor.redColor().CGColor
+                layer.borderWidth = 1.0
+            } else {
+                clipsToBounds = true
+            }
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initFloatingImageView()
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initFloatingImageView()
+    }
+    
+    func initFloatingImageView() {
+        floatingImage = UIImageView(frame: CGRectZero)
+        addSubview(floatingImage)
+    }
+    
+    func setupWithImage(newImage: UIImage, horizontalImages: Int, verticalImages: Int) {
+    
+        if !debug {
+            clipsToBounds = true
+        }
+        
+        floatingImage.frame = CGRectMake(0, 0, frame.width * CGFloat(horizontalImages), frame.height * CGFloat(verticalImages))
+        
+        floatingImage.image = newImage
+        
+        self.horizontalImages = horizontalImages
+        self.verticalImages = verticalImages
+        
+        floatingImage.contentMode = UIViewContentMode.TopLeft
+        
+        var index = 1
+        
+        var tempX = 1
+        
+        var tempY = 0
+        
+        while index <= (horizontalImages * verticalImages - 1) {
+
+            positionMatrix.append(CGPoint(x: tempX, y: tempY))
+
+            if tempX + 1 > horizontalImages - 1 {
+                tempX = 0
+                tempY += 1
+            } else {
+                tempX += 1
+            }
+            
+            index += 1
+        }
+        
+        println("\(horizontalImages) \(verticalImages)")
+        
+        println(positionMatrix)
+        
+    }
+    
+    func locateFrame(frameIndex: Int) -> CGPoint {
+
+        var location: CGPoint = positionMatrix[frameIndex]
+        
+        return location
+    }
+    
+    func toNewFrame(frameIndex: Int) {
+        
+        if frameIndex + 1 <= positionMatrix.count {
+            currentIndex = frameIndex
+        } else {
+            currentIndex = 0
+        }
+        
+        var location = locateFrame(currentIndex)
+        
+        floatingImage.frame = CGRectMake(-location.x * frame.width, -location.y * frame.height, floatingImage.frame.width, floatingImage.frame.height)
+    }
+    
+    func startNaughtyAnimation() {
+        naughtyAnimating = true
+        displayLink = CADisplayLink(target: self, selector: Selector("callbackNaughtyAnimation"))
+        displayLink.addToRunLoop(NSRunLoop.currentRunLoop(), forMode: NSRunLoopCommonModes)
+    }
+    
+    func callbackNaughtyAnimation() {
+        
+        if frameCount == fps {
+            toNewFrame(currentIndex + 1)
+            frameCount = 0
+        } else {
+            frameCount += 1
+        }
+
+        
+    }
+    
+    func stopNaughtyAnimation() {
+        naughtyAnimating = false
+        displayLink.invalidate()
+    }
+}
--- NaughtyImageView/ViewController.swift
+++ NaughtyImageView/ViewController.swift
@@ -10,8 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    
+    var starImageView = NaughtyImageView(frame: CGRectZero)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        starImageView.frame = CGRectMake(view.frame.width/2.0 - 36.5/2.0, 100, 36.5, 37.5)
+        
+        starImageView.debug = false
+        
+        view.addSubview(starImageView)
+        
+        starImageView.setupWithImage(UIImage(named: "star")!, horizontalImages: 6, verticalImages: 4)
+        
+        slider.maximumValue = Float(starImageView.horizontalImages * starImageView.verticalImages) - 1
+        
+        slider.value = 0
+        
+        slider.continuous = true
+        
+        slider.addTarget(self, action: "valueChanged:", forControlEvents: UIControlEvents.ValueChanged)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +39,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func valueChanged(slider: UISlider) {
+        
+        var frameIndex = Int(slider.value)
+        
+        starImageView.toNewFrame(frameIndex)
+        
+        
+    }
 
+    @IBAction func start(sender: AnyObject) {
+        if starImageView.naughtyAnimating {
+            starImageView.stopNaughtyAnimation()
+        } else {
+            starImageView.startNaughtyAnimation()
+        }
+
+    }
 
 }
 
